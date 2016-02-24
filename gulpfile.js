fac90eb3afe4b68a7663980a8c3ad7f650f95296
Corey McDaniel <corey.r.mcdaniel@live.com>
2016-02-23 16:01:33 -0800

    Render 3d model.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var path = require('path');
+var plugins = require('gulp-load-plugins')({
+	pattern: ['gulp-*', 'gulp.*'],
+	replaceString: /\bgulp[\-.]/
+});
+
+/*********************************************
+* TASK: gulp
+* Start server and watch for file changes
+*********************************************/
+gulp.task('default', ['connect:dev'], function () {
+	gulp.watch('./index.html', ['reload']);
+	gulp.watch('/css/main.css', ['reload']);
+});
+
+
+/*********************************************
+* TASK: connect:dev
+* Start development server
+*********************************************/
+gulp.task('connect:dev', function() {
+	plugins.connect.server({
+		root: path.resolve('./'),
+		port: 3000,
+		livereload: true
+	});
+});
+
+/*********************************************
+* TASK: reload
+* Reload server
+*********************************************/
+gulp.task('reload', function () {
+	gulp.src('./index.html')
+		.pipe(plugins.connect.reload());
+});
+
+
